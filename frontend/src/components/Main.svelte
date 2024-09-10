dac7cf721203436ff11b29dff49f4d29062aa23c
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-10 23:06:29 +0300

    frontend development

---
+++ frontend/src/components/Main.svelte
@@ -0,0 +1,44 @@
+<script>
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+  <section id="introPage" class="grid grid-cols-1 py-8 sm:py-14">
+    <div
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+    >
+      <h2 class="font-semibold text-center text-4xl sm:text-5xl md:text-6xl">
+        <span class="poppins text-violet-400">Lid</span>Tek
+      </h2>
+      <p class="text-base text-center sm:text-lg md:text-cl">
+        <span class="text-violet-400">Techs</span> That We Use
+      </p>
+      <div class="flex flex-col sm:-32"></div>
+      <a
+        href="https://github.com/LidTek"
+        target="_blank"
+        class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+      >
+        <i class="fa-brands fa-github"></i>
+        <p>Check Our Github</p>
+      </a>
+      <div class="grid grid-cols-2 gap-12 lg:gap-10"></div>
+    </div>
+  </section>
+  <!-- <section
+    id="about"
+    class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+  >
+    <div
+      class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+    >
+      <h6 class="text-lg sm:text-xl md:text-2xl">Want To Know More?</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        A Bit <span class="poppins text-violet-400">About</span> Me
+      </h3>
+    </div>
+    <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+      I am . . .
+    </p>
+    <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]"></div>
+  </section> -->
+</main>
