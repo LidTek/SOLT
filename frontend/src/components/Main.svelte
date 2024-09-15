a892adf62665a27e8c315fd3598be00c865fce24
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-15 23:11:06 +0300

    new design

--- frontend/src/components/Main.svelte
+++ frontend/src/components/Main.svelte
@@ -1,40 +1,13 @@
 <script>
 </script>
 
-<main class="flex flex-col flex-1 p-4">
-  <section id="introPage" class="grid grid-cols-1 py-8 sm:py-14">
-    <div
-      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+<main>
+  <div class="flex flex-col items-center">
+    <h1
+      class="py-4 font-bold
+    text-5xl"
     >
-      <h2 class="font-semibold text-center text-4xl sm:text-5xl md:text-6xl">
-        <span class="poppins text-violet-400">Lid</span>Tek
-      </h2>
-      <p class="text-base text-center sm:text-lg md:text-cl">
-        <span class="text-violet-400">Techs</span> That We Use
-      </p>
-      <div class="flex flex-col sm:-32"></div>
-      <a
-        href="https://github.com/LidTek"
-        target="_blank"
-        class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
-      >
-        <i class="fa-brands fa-github"></i>
-        <p>Check Our Github</p>
-      </a>
-      <div class="grid grid-cols-2 gap-12 lg:gap-10"></div>
-    </div>
-  </section>
+      <span class="font-bold poppins text-blue-600">Draw</span>niverse
+    </h1>
+  </div>
 </main>
-
-<style>
-  main {
-    font-family: sans-serif;
-    text-align: center;
-  }
-
-  :global(body) {
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-  }
-</style>
