b8ed8ae4d61a661b01eb121727b708be01e0e4bc
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-15 23:48:16 +0300

    new design

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -23,13 +23,15 @@
     {/each} -->
     <a
       target="_blank"
-      href="https://x.com/temidaradev"
+      href="https://discord.gg/fmYnZss29N"
       class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
     >
       <div
         class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
       ></div>
-      <h4 class="relative z-9">Get In Touch</h4>
+      <h4 class="relative z-9">
+        Join Our Discord <i class="fa-brands fa-discord"></i>
+      </h4>
     </a>
   </div>
 </header>
