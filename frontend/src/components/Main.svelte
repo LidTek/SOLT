79f4c483f82fb736e0c0650b0030c44f6fd0a845
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-18 23:45:34 +0300

    will style

--- frontend/src/components/Main.svelte
+++ frontend/src/components/Main.svelte
@@ -2,7 +2,6 @@
 </script>
 
 <main>
-  <div class="box"></div>
   <div class="flex flex-col items-center">
     <h1
       class="py-4 font-bold
@@ -11,4 +10,7 @@
       <span class="font-bold poppins text-blue-600">Draw</span>niverse
     </h1>
   </div>
+  <div
+    class="py-20 sm:py-32 bg-black rounded-r-full border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+  ></div>
 </main>
