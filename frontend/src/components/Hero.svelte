77340c864f153c2dcd81c74ed1cd257ae24b013e
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-26 21:43:54 +0300

    footer ignored

--- frontend/src/components/Hero.svelte
+++ frontend/src/components/Hero.svelte
@@ -1,26 +1,24 @@
 <script></script>
 
 <hero
-  class="z-[10] top-0 duration-200 px-6 py-6 flex items-center justify-center"
+  class="absolute sticky z-[10] top-0 duration-200 px-6 py-6 flex items-center justify-center"
 >
-  <div
-    class=" py-10 sm:py-10 px-20 max-w-[1400px] bg-black rounded-full flex flex-col gap-4 sm:gap-8 justify-center items-center opacity-70"
-  >
+  <div class="py-10 sm:py-20 bg-black max-w-[700px] flex flex-col items-center">
     <div class="mb-6">
       <label
-        for="username"
-        class="block mb-2 text-sm font-medium text-violet-700 dark:text-violet-500 opacity-100"
+        for="success"
+        class="block mb-2 text-sm font-medium text-green-700 dark:text-green-500"
         >Your name</label
       >
       <input
         type="text"
-        id="username"
-        class="bg-violet-50 border border-violet-500 dark:text-violet-400 placeholder-violet-700 dark:placeholder-violet-500 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-violet-500"
+        id="success"
+        class="bg-violet-50 border border-violet-500 dark:text-violet-400 placeholder--700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
         placeholder="Enter A Legit Name"
       />
     </div>
     <button
-      class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative justify-center items-center overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+      class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative justify-center overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
     >
       <div
         class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,4 +6,4 @@
 
 <Main />
 <Hero />
-<Footer />
+<!-- <Footer /> -->
