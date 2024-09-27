ecb5d0f1f29ed5387debf4a6e116a6075faff36f
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-27 22:19:27 +0300

    signup deleted

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -13,7 +13,7 @@
   <div class="sm:flex flex wrap items-center content-end space-x-4">
     <a
       target="_blank"
-      href="/sign-up"
+      href=""
       class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
     >
       <div
