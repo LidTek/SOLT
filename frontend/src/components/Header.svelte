516595b21313a9045587d52bcb927b1c7ea153fe
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-27 22:16:57 +0300

    typo fixed

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -13,7 +13,7 @@
   <div class="sm:flex flex wrap items-center content-end space-x-4">
     <a
       target="_blank"
-      href="/sign_up"
+      href="/sign-up"
       class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
     >
       <div
