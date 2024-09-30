ae26b05b24dfa54f676e00fe15d4cd8301428f60
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-09-30 23:16:29 +0300

    test

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -1,5 +1,7 @@
 <script>
   export let y;
+  import { signIn, signOut } from "@auth/sveltekit/client";
+  import { page } from "$app/stores";
 </script>
 
 <header
@@ -23,15 +25,17 @@
         <p>Sign Up</p>
       </h4>
     </a> -->
-    <a
-      target="_blank"
-      href="/login"
-      class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
-    >
-      <div
-        class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
-      ></div>
-      <h4 class="relative z-9"><p>Login</p></h4>
-    </a>
+    {#if $page.data.session?.user}
+      <p>Signed in as {$page.data.session.user.email}</p>
+      <button on:click={signOut}>Sign out</button>
+    {:else}
+      <p>Not signed in.</p>
+      <button class="bg-black rounded-full" on:click={() => signIn("google")}
+        >Sign In With Google <i class="fa-brands fa-google"></i></button
+      >
+      <button class="bg-black rounded-full" on:click={() => signIn("github")}
+        >Sign In With Github <i class="fa-brands fa-github"></i></button
+      >
+    {/if}
   </div>
 </header>
