e634bd1e97a245c1ded5cea19e0467a360537709
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-10-02 22:11:30 +0300

    comments deleted waiting for firebase

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -13,18 +13,6 @@
     >
   </h1>
   <div class="sm:flex flex wrap items-center content-end space-x-4">
-    <!-- <a
-      target="_blank"
-      href=""
-      class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
-    >
-      <div
-        class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
-      ></div>
-      <h4 class="relative z-9">
-        <p>Sign Up</p>
-      </h4>
-    </a> -->
     {#if $page.data.session?.user}
       <p>Signed in as {$page.data.session.user.email}</p>
       <button on:click={signOut}>Sign out</button>
