61b8758b2d04fbf1f0830c267e7278c5ed24a57b
kettek <kts@kettek.net>
2024-10-04 14:06:48 -0700

    Add trustHost

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,17 +1,18 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from '@auth/sveltekit/providers/github'
-import Google from "@auth/core/providers/google"
+import { SvelteKitAuth } from "@auth/sveltekit";
+import GitHub from "@auth/sveltekit/providers/github";
+import Google from "@auth/core/providers/google";
 
 import {
-    GITHUB_ID,
-    GITHUB_SECRET,
-    GOOGLE_ID,
-    GOOGLE_SECRET,
-} from "$env/static/private"
+  GITHUB_ID,
+  GITHUB_SECRET,
+  GOOGLE_ID,
+  GOOGLE_SECRET,
+} from "$env/static/private";
 
 export const { handle } = SvelteKitAuth({
+  trustHost: true, // Added for reverse proxy on gamu
   providers: [
-    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET}),
-    Google({clientId:GOOGLE_ID, clientSecret: GOOGLE_SECRET}),
+    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
+    Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET }),
   ],
-})
+});
